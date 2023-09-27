8183a184183884f94eb50c56f486b99af4370c1f
Oksana Paygildina <kazaikina1997@mail.ru>
2023-09-27 22:28:45 +0300

    ispravlena peremennay

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         int amount = 13676; // стоимость билета
+        int quantity = 1; // 
         int miles = 1; //количество миль
-        boolean quantity = true;
 
-        if (quantity) {
+        if (amount > 20) {
             miles = 1;
         }
         int bonus = amount / 20; //количество бонусных миль
