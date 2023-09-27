ebf05abca4292d41ccfdb4d1fd711cfb8fcc4d0d
Oksana Paygildina <kazaikina1997@mail.ru>
2023-09-27 13:16:47 +0300

    inital commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 13676;
+        int miles = 1;
+         boolean quantity = true;
+
+        if (quantity){
+            miles = 1;
+        }
+        int bonus = amount / 20 ;
+        System.out.println("Начисленные мили: " + bonus);
+
+
+    }
+}
