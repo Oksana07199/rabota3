701c3682b6c705e4f956f7bfd38bd35e0373cee9
Oksana Paygildina <kazaikina1997@mail.ru>
2023-09-27 21:00:22 +0300

    opisanie koda

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int amount = 13676;
-        int miles = 1;
+        int amount = 13676; // стоимость билета
+        int miles = 1; //количество миль
         boolean quantity = true;
 
         if (quantity) {
             miles = 1;
         }
-        int bonus = amount / 20;
-        System.out.println("Начисленные мили: " + bonus);
+        int bonus = amount / 20; //количество бонусных миль
+        System.out.println("Начисленные мили: " + bonus); //вывод результата
 
 
     }
