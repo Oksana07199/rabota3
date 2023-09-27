3fc7eaf0356112a0230ec89935c246f5bacf7773
Oksana Paygildina <kazaikina1997@mail.ru>
2023-09-27 13:41:38 +0300

    commit3

---
+++ src/Main2.java
@@ -0,0 +1,14 @@
+public class Main2 {
+    public static void main(String[] args) {
+        int startingScore = 100; // начальный счет
+        int replenishment = 2200; // сумма пополнения
+
+        int bonus;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
+        } else {
+            bonus = 0;
+        }
+        System.out.println("Итоговая сумма:" + bonus);
+    }
+}
